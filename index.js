09d3d12f7114d60f481da5b43e36a8a44e510866
Sharon Njoroge <waithirasharon222@gmail>
2025-09-28 15:20:52 +0300

    add work

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+
+function submitData(name, email) {
+      return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+         "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+        email: email
+    })
+  })
+       .then(response => response.json())
+    .then(object => {
+         
+
+      const body = document.querySelector("body");
+      
+      const p = document.createElement("p");
+      p.textContent = `User created with ID: ${object.id}`;
+      body.appendChild(p);
+    })
+    .catch(error => {
+       
+
+
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = error.message;
+     
+     
+      body.appendChild(p);
+    });
+}
